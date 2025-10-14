29b4f5c6922cd8ed88b1ad8de16b61c44ae8c778
zhyuk <pc9445@naver.com>
2025-10-14 23:25:00 +0900

    fix: update App.jsx after rollback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -30,3 +31,5 @@ function App() {
     </>
   )
 }
+
+export default App
