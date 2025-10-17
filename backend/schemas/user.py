9af93661066af4e926136b60a3cfc9bd166a541a
zhyuk <pc9445@naver.com>
2025-10-17 16:09:20 +0900

    chore: 스키마, 모델 파일 분리작업

--- backend/schemas/user.py
+++ backend/schemas/user.py
@@ -10,15 +10,18 @@ class UserBase(BaseModel):
     birth: Optional[datetime] = None
     gender: str
 
+# 생성 스키마
 class UserCreate(UserBase):
     pass
 
+# 수정 스키마
 class UserUpdate(BaseModel):
     id: int
     password: Optional[str] = None
     nickname: Optional[str] = None
     gender: Optional[str] = None
 
+# 응답 스키마
 class UserResponse(UserBase):
     id: int
     created_at: datetime
