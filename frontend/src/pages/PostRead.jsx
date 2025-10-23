458a6c4bff622bf3803703e761223080e4db0ea4
hyojeong <shj000323@gmail.com>
2025-10-23 22:51:16 +0900

    PostCreate/PostRead/PostUpdate.jsx 폴더 변경 및 수정

---
+++ frontend/src/pages/PostRead.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+function PostRead() {
+    return (
+        <>
+            <h3>카테고리(대분류)</h3>
+            <h4>카테고리(소분류)</h4>
+            <div>
+                Id
+                수정 삭제 버튼
+                created_date
+                title
+                content
+                reaction
+            </div>
+        </>
+    )
+}
+
+export default PostRead;
