511b18a06ace495f4a49a11d63da322494eaca56
Hyojeong <shj000323@gmail.com>
2025-10-24 14:43:14 +0900

    ProtectedRoute 추가

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { Navigate } from "react-router-dom";
+
+/**
+ * 로그인 여부를 확인해 보호된 페이지 접근을 제어하는 컴포넌트
+ * 
+ * @param {boolean} isAuthenticated - 로그인 여부 (true/false)
+ * @param {JSX.Element} element - 렌더링할 컴포넌트 (로그인된 경우)
+ */
+function ProtectedRoute({ isAuthenticated, element }) {
+  // 로그인 안 되어 있으면 로그인 페이지로 이동
+  if (!isAuthenticated) {
+    alert("로그인 후 이용 가능합니다.");
+    return <Navigate to="/login" replace />;
+  }
+
+  // 로그인 되어 있으면 원래 페이지 렌더링
+  return element;
+}
+
+export default ProtectedRoute;
